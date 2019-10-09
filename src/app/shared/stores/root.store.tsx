1b92c9b830b7ffbf8b911cd0e9a7359a0c441101
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2019-10-09 12:21:23 +0200

    Added user interface store, and checked its functionality.

--- src/app/shared/stores/root.store.tsx
+++ src/app/shared/stores/root.store.tsx
@@ -1,11 +1,13 @@
 import { RouterState, RouterStore } from 'mobx-state-router';
 
 import routes from '../routing/routes.routing';
+import UserInterfaceStore from './userInterface.store';
 
 const notFound = new RouterState('notFound');
 
 export default class RootStore {
   routerStore = new RouterStore(this, routes, notFound);
+  userInterfaceStore = new UserInterfaceStore(this);
 }
 
 export const rootStore = new RootStore();
---
+++ src/app/shared/stores/userInterface.store.tsx
@@ -0,0 +1,24 @@
+import { decorate, observable, computed, action } from 'mobx';
+import RootStore from './root.store';
+
+export default class UserInterfaceStore {
+  overlay: boolean = false;
+
+  constructor(private rootStore: RootStore) {}
+
+  get overlayStatus(): boolean {
+    return this.overlay;
+  }
+
+  toggleOverlay(): void {
+    this.overlay = !this.overlay;
+  }
+}
+
+decorate(UserInterfaceStore, {
+  overlay: observable,
+
+  overlayStatus: computed,
+
+  toggleOverlay: action
+});
